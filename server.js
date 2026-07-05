db0059a59a9461c347d193207708318e88f2d391
Jacob Banducci <jbanducci321@gmail.com>
2026-07-05 16:04:34 -0700

    Made small change to fix issue between express and render proxy

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
